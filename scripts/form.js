72fdea613a6477b1eeb1fd1a5a4795e5716530fe
JimIstook <jimistook@gmail.com>
2025-10-08 06:08:36 -0600

    made product review form

---
+++ scripts/form.js
@@ -0,0 +1,28 @@
+const currentYearElement = document.getElementById("currentyear");
+const currentYear = new Date().getFullYear();
+const lastModifiedElement = document.getElementById("lastmodified");
+
+if (currentYearElement) {
+    currentYearElement.textContent = currentYear;
+}
+
+if (lastModifiedElement) {
+    lastModifiedElement.textContent = `Last Modification: ${document.lastModified}`;
+}
+
+const products = [
+    { id: "fc-1888", name: "Flux Capacitor", averagerating: 4.5 },
+    { id: "fc-2050", name: "Power Laces", averagerating: 4.7 },
+    { id: "fs-1987", name: "Time Circuits", averagerating: 3.5 },
+    { id: "ac-2000", name: "Low Voltage Reactor", averagerating: 3.9 },
+    { id: "jj-1969", name: "Warp Equalizer", averagerating: 5.0 }
+];
+
+const selectElement = document.getElementById("productName");
+
+products.forEach(product => {
+    let option = document.createElement("option");
+    option.value = product.id;
+    option.textContent = product.name;
+    selectElement.appendChild(option);
+});
